2816178f8f97ec97571a77f63b416bcb6f39ddbe
Efemena <88979259+Festivemena@users.noreply.github.com>
2025-01-16 05:15:32 +0100

    Add files via upload

--- controllers/admin/withdrawal-checklist.js
+++ controllers/admin/withdrawal-checklist.js
@@ -1,4 +1,4 @@
-const User = require("../../models/user");
+const Transaction = require("../../models/transactions");
 
 const { CustomAPIError } = require("../../errors/custom-error");
 const asyncWrapper = require("../../middleware/async");
@@ -10,16 +10,16 @@ const modifyWithdrawalChecklist = asyncWrapper(async (req, res, next) => {
   console.log(updatedData)
   console.log(id)
 
-  const updatedUser = await User.findByIdAndUpdate(id, updatedData, {
+  const updatedTransaction = await Transaction.findByIdAndUpdate(id, updatedData, {
     new: true, // Returns the modified document rather than the original
     runValidators: true, // Ensures validations are run on the update operation
   });
 
-  if (!updatedUser) {
-    throw new CustomAPIError("User not found", 404);
+  if (!updatedTransaction) {
+    throw new CustomAPIError("Transaction not found", 404);
   }
 
-  res.status(200).json({ updatedUser });
+  res.status(200).json({ updatedTransaction });
 });
 
 module.exports = modifyWithdrawalChecklist;
