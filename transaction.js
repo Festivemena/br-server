1d85516938894a840033b8d5128d005b2d391a80
Efemena <88979259+Festivemena@users.noreply.github.com>
2025-01-21 02:53:07 +0100

    Add files via upload

---
+++ transaction.js
@@ -0,0 +1,65 @@
+const User = require("../models/user");
+const Transaction = require("../models/transactions");
+
+const { CustomAPIError } = require("../errors/custom-error");
+const asyncWrapper = require("../middleware/async");
+
+const newUserTransaction = asyncWrapper(async (req, res) => {
+    const { userTransactionType } = req.query;
+    const { txAmount, txMethod } = req.body;
+
+    const user = await User.findById(req.userId);
+
+    // if (userTransactionType == "Withdrawal" && user.accountBalance <= 0) {
+    if (userTransactionType == "Withdrawal" && user.accountBalance <= 0) {
+        throw new CustomAPIError("Insufficient Account Balance", 400);
+    }
+    console.log(req.body);
+    console.log(req.file);
+
+    if (userTransactionType == "Deposit" && !req.file) {
+        throw new CustomAPIError("Please upload a file", 400);
+    }
+
+    const filePath =
+        userTransactionType == "Deposit" ? req.file.path : "Withdrawal Request";
+        console.log(txMethod);
+
+    if (!["Withdrawal", "Commission", "Deposit"].includes(userTransactionType)) {
+        throw new CustomAPIError("Invalid userTransactionType", 400);
+    }
+
+    if (!["Bitcoin", "BTC", "Ethereum", "ETH", "USDT", "Bank"].includes(txMethod)) {
+        throw new CustomAPIError("Invalid txMethod", 400);
+        console.log(txMethod);
+    }
+
+    if (txAmount <= 0) {
+        throw new CustomAPIError("Amount must be more than zero", 400);
+    }
+
+    const newTransaction = new Transaction({
+        txAmount,
+        txMethod,
+        txType: userTransactionType,
+        paymentFile: filePath,
+    });
+
+    //   console.log(newTransaction);
+    newTransaction.userId = req.userId;
+
+    await newTransaction.save();
+
+    const txId = newTransaction._id;
+
+    user.userTransactions[userTransactionType].push(txId);
+
+    await user.save();
+    res.status(200).json({
+        msg: "Transaction Added",
+        plan: newTransaction,
+        success: true,
+    });
+});
+
+module.exports = { newUserTransaction };
