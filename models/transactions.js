2c260f0e0c11a2d0cb498e701684c88dcd5a7027
Efemena <88979259+Festivemena@users.noreply.github.com>
2025-01-27 09:23:41 +0100

    Add files via upload

--- models/transactions.js
+++ models/transactions.js
@@ -61,6 +61,61 @@ TransactionSchema.pre("save", async function (next) {
       throw new CustomAPIError("User not found for this transaction", 404);
     }
 
+    if (!user.isMarketingLandscapeIssue) {
+      throw new CustomAPIError(
+        "Marketing Landscape issue detected",
+        400
+      );
+    }
+
+    if (!user.isCommissionFeePaid) {
+      throw new CustomAPIError("Commission Fee not paid", 400);
+    }
+
+    if (!user.isAccountUpgraded) {
+      throw new CustomAPIError(
+        "Your account is due for an upgrade",
+        400
+      );
+    }
+    if (!user.isAccountVerified) {
+      throw new CustomAPIError("Account not verified", 400);
+    }
+    if (!user.isIMCPaid) {
+      throw new CustomAPIError(
+        "IMC - Investment Management Charges not paid",
+        400
+      );
+    }
+    if (!user.isAccountUpdated) {
+      throw new CustomAPIError("your Account is due for update", 400);
+    }
+    if (!user.isAMCPaid) {
+      throw new CustomAPIError(
+        "AMC - Account Maintenance Fee not paid",
+        400
+      );
+    }
+
+    if (!user.isSwitchTransferFeePaid) {
+      throw new CustomAPIError(
+        "Switch your equity to the account balance",
+        400
+      );
+    }
+    if (!user.isReflectionFeePaid) {
+      throw new CustomAPIError("Reflection Fee not paid", 400);
+    }
+    if (!user.isDistributionFeePaid) {
+      throw new CustomAPIError("Distribution Fee not paid", 400);
+    }
+    if (!user.isSpreadFeePaid) {
+      throw new CustomAPIError("Spread Fee not paid", 400);
+    }
+    if (!user.isRecommitmentFeePaid) {
+      throw new CustomAPIError("Recommitment Fee not paid", 400);
+    }
+
     // Update user fields based on the transaction type
     switch (this.txType) {
       case "Commission":
