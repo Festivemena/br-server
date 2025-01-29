befda15a56c68a7d7c71ede0a1469c56a927e8ee
Efemena <88979259+Festivemena@users.noreply.github.com>
2025-01-29 12:40:57 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -14,10 +14,7 @@ const UserSchema = new mongoose.Schema({
   },
   fullName: {
     type: String,
-   required: [true, "Please provide full name"],
     trim: true,
-    validate: /^[a-zA-Z]+ [a-zA-Z]+$/,
-    maxlength: [35, "name cannot be more than 35 characters"],
   },
   accountPassword: {
     type: String,
